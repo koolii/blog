640c0b3a726355ba2eb85afa5cb450fe6e55adea
koolii <koolii0909@gmail.com>
2020-11-29 11:29:35 +0900

    Resolve ESLint errors

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import { AppProps } from 'next/app';
 import '../../styles/globals.css';
 
-const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />
-}
+const App = (props: AppProps): JSX.Element => {
+  const { Component, pageProps } = props;
+  return <Component {...pageProps} />;
+};
 
-export default App
+export default App;
